dc1fcda436bd16f7eecbbdd101055d3fd06d108c
roman <escaro@ukr.net>
2021-03-01 08:07:03 +0200

    initial commit

---
+++ callbacks/script.js
@@ -0,0 +1,10 @@
+/*  example blocking's programs */
+
+console.log("Before");
+let start = new Date().getTime();
+while (new Date().getTime() - start < 4000);
+console.log("After");
+
+/*  ================ callbacks ============  */
+
+/*  ================ promises ============  */
---
+++ example/script.js
@@ -0,0 +1,15 @@
+const pokemonEl = document.getElementById("pokemon");
+const btn = document.getElementById("btn");
+const loader = document.getElementById("loader");
+const total = document.getElementById("total");
+const URL = "https://pokeapi.co/api/v2/pokemon/";
+let id = 0;
+
+document.addEventListener("click", handler);
+
+function handler(e) {}
+
+/* 
+https://img.pokemondb.net/artwork/squirtle.jpg
+
+*/
